ad74e94653aea2fa91d4742296a5f4b302154e9b
Joey Piccola <joey.piccola@gmail.com>
2022-05-31 23:16:49 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # iterable-testing
+
+This is a quick POC of using Terraform to generate AWS certificates, route53 records, and a cloud front distribution in support of Iterable HTTPS click-tracking.
+
+## general notes
+
+- A single certificate is created with SANs in support of multiple click tracking URLs.
+- ACM automatically writes DNS challenges to route53.
+- A single cloud front distribution is created leveraging the previously mentioned certificate.
+- The single cloud front distribution is configured with aliases matching the CN and all SANs on the previously mentioned certificate.
+- Route53 CNAME records are created for the CN and all SANs on the previously mentioned certificate. Their values is the cloud front distribution domain name.
+
+## terraform notes
+
+- The first item in the `local.sub_domains` list is used as the CN in the certificate. Remaining items are used as SANs.
+- See `provider.tf` for the AWS region used.
---
+++ main.tf
@@ -0,0 +1,130 @@
+locals {
+  name                   = "one.joeypiccola-aws.com"
+  iterable_origin_domain = "links.iterable.com"
+  iterable_origin_id     = "ELB-linkslb-1505033010"
+  route53_zone           = "joeypiccola-aws.com"
+  sub_domains            = ["itr-links","itr-links.dev","itr-images","itr-images.dev"]
+  fqdns                  = [for k in local.sub_domains : format("%s.${local.route53_zone}",k)]
+  tags                   = {app = "iterable"}
+}
+
+data "aws_route53_zone" "route53_zone" {
+  name         = local.route53_zone
+  private_zone = false
+}
+
+# create the cert, use first fqdn in fqdns list as CN and remaining fqdns as SANs
+resource "aws_acm_certificate" "acm_certificate" {
+  domain_name               = element(local.fqdns, 0)                  # <-- CN
+  subject_alternative_names = slice(local.fqdns,1,length(local.fqdns)) # <-- SANs
+  validation_method         = "DNS"
+  tags                      = local.tags
+}
+
+# loop over each cert domain validation option (dvo) and create the appropriate route53 record
+resource "aws_route53_record" "route53_record_validation_cname" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.route53_zone.zone_id
+}
+
+# use this fancy resource to watch and wait for the validation proccess to complete
+resource "aws_acm_certificate_validation" "certificate_validation" {
+  certificate_arn         = aws_acm_certificate.acm_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.route53_record_validation_cname : record.fqdn]
+}
+
+resource "aws_cloudfront_cache_policy" "cache_policy" {
+  name        = "iterable-cache-policy"
+  comment     = "Iterable cache policy with caching disabled."
+  default_ttl = 0
+  max_ttl     = 0
+  min_ttl     = 0
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_request_policy" "origin_request_policy" {
+  name    = "iterable-origin-request-policy"
+  comment = "Iterable origin request policy that sends cookies, headers, and query strings to the origin."
+
+  cookies_config {
+    cookie_behavior = "all"
+  }
+  headers_config {
+    header_behavior = "allViewer"
+  }
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+}
+
+resource "aws_cloudfront_distribution" "cfd" {
+  aliases         = local.fqdns
+  enabled         = true
+  is_ipv6_enabled = true
+  tags            = local.tags
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.acm_certificate.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  origin {
+    domain_name = "links.iterable.com"
+    origin_id   = "ELB-linkslb-1505033010"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods          = [ "GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE" ]
+    cache_policy_id          = aws_cloudfront_cache_policy.cache_policy.id
+    cached_methods           = [ "GET", "HEAD" ]
+    target_origin_id         = "ELB-linkslb-1505033010" # this matches the origin_id in the defined origin {} block
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.origin_request_policy.id
+    viewer_protocol_policy   = "allow-all"
+  }
+}
+
+resource "aws_route53_record" "route53_record_cfd_cname" {
+  for_each = toset(local.fqdns)
+
+  zone_id = data.aws_route53_zone.route53_zone.zone_id
+  name    = each.key
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_cloudfront_distribution.cfd.domain_name]
+}
